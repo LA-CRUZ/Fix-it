a5321db28740064cf7f83c17287d24ecfc7651f4
lucasgivord <48027211+lucasgivord@users.noreply.github.com>
2020-02-01 02:10:38 +0100

    creation of the main screen with redirection to the other screens

---
+++ Assets/Scripts/BtnManager/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void SwitchSceneBtn (string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+
+    public void ExitGameBtn ()
+    {
+        Application.Quit();
+    }
+}
